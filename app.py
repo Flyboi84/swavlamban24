d24db33104e61ffc2b07fa93231a24ccae81215a
AV <100195998+Flyboi84@users.noreply.github.com>
2024-10-01 14:47:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import sqlite3
-from streamlit_authenticator import Authenticate
+#from streamlit_authenticator import Authenticate
+import streamlit_authenticator as stauth
 
 # Initialize database connection
 conn = sqlite3.connect('swavlamban24_registration.db')
@@ -28,7 +29,7 @@ credentials = {
 authenticator = Authenticate(credentials)
 
 # Login section
-org_name = authenticator.login(form_name="Login", location="main")
+org_name = authenticator.login(form_name="Login")#, location:'main')
 
 
 if org_name:
